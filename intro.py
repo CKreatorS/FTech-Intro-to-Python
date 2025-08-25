896d6fd87d595fde0fc9d2835690639d0dbcab9f
Cai <caitlyn.morales424@gmail.com>
2025-08-25 12:31:04 -0400

    intro

--- intro.py
+++ intro.py
@@ -1,1 +1,10 @@
 print("Hello World!")
+    # prints Hello World!
+
+myVariable = 8*9
+print(myVariable)
+    # prints the value stored in myVariable
+print("myVariable")
+    # prints myVariable
+
+input("Enter into terminal: ")
