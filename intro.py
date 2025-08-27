aae082e55055c06810a1ee527aa2e116ac7e673b
Cai <caitlyn.morales424@gmail.com>
2025-08-27 12:34:19 -0400

    notes

--- intro.py
+++ intro.py
@@ -1,15 +1,21 @@
-print("Hello World!")
-    # prints Hello World!
+# print("Hello World!")
+#     # prints Hello World!
 
-myVariable = 8*9
-print(myVariable)
-    # prints the value stored in myVariable
-print("myVariable")
-    # prints myVariable
+# myVariable = 8*9
+# print(myVariable)
+#     # prints the value stored in myVariable
+# print("myVariable")
+#     # prints myVariable
+# input("Enter into terminal: ")
+#     # creates an input area in the terminal''
 
-input("Enter into terminal: ")
-    # creates an input area in the terminal''
+# myName = input("Type your name: ")
+#     # assigns myName to the input
+# print(myName)
+#      # prints the name entered into input
 
-myName = input("Type your name: ")
-print(myName)
-
+input_name = input("Hello, what is your name?\n").strip()
+    # input takes a str name after the written str on a new line \n
+    # strip removes unecessary whitespace from beginning and end of input
+print("Hello " + input_name)
+    # prints the inputted name after the written str
