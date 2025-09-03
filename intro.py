eaaccaab3ff2f1042f8f02e7f8354fdd7a16d23b
Cai <caitlyn.morales424@gmail.com>
2025-09-03 12:42:18 -0400

    9/3

--- intro.py
+++ intro.py
@@ -14,8 +14,27 @@
 # print(myName)
 #      # prints the name entered into input
 
-input_name = input("Hello, what is your name?\n").strip()
-    # input takes a str name after the written str on a new line \n
-    # strip removes unecessary whitespace from beginning and end of input
-print("Hello " + input_name)
-    # prints the inputted name after the written str
+# input_name = input("Hello, what is your name?\n").strip()
+#     # input takes a str name after the written str on a new line \n
+#     # strip removes unecessary whitespace from beginning and end of input
+# print("Hello " + input_name)
+#     # prints the inputted name after the written str
+
+# if statements, if-then statements, if-else statements, if-then-elif
+
+# grade = 100
+# # if (grade > 60):
+# #     print("You pass the class.")
+# # else:
+# #     print("You fail.")
+
+# if(grade >= 90):
+#     print("A")
+# elif(grade >= 80):
+#     print("B")
+# elif(grade >= 70):
+#     print("C")
+# elif(grade >= 60):
+#     print("D")
+# else:
+#     print("F")
