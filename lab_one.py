b1c36d4f895aa36b8545817a11f4e4ec0c33446f
Cai <caitlyn.morales424@gmail.com>
2025-09-08 12:17:25 -0400

    lab one

---
+++ lab_one.py
@@ -0,0 +1,33 @@
+"""
+Caitlyn Morales
+Created: 09/08/2025
+Edited: 09/08/2025
+
+Created a Python script that implements a simple units converter. 
+Program first asks to pick from a menu of conversions. Then it asks for the input, 
+performs the conversion, and displays the result.
+"""
+
+print("Welcome to Converter")
+
+selection = input("Please select from the following converting options: ")
+print("""
+      (1)Kelvin to Farenheit
+      (2)Farenheit to Kelvin
+      (3)Meters to Yards
+      (4)Yards to Meters
+      (5)Quit
+    """)
+match selection:
+    case '1':
+        print("")
+    case '2':
+        print("")
+    case '3':
+        print("")
+    case '4':
+        print("")
+    case '5':
+        print("")
+
+    
