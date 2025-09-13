1a959b3d0469b99a278570ad2f37658592278edf
Cai <caitlyn.morales424@gmail.com>
2025-09-13 12:44:16 -0400

    new lab

--- lab_one.py
+++ lab_one.py
@@ -24,32 +24,37 @@ print(options)
 # Print options
 selection = input("Please select from the following converting options: ")
 # Create input for match-case called selection
-
-match selection:
-    case '1':
+while (selection <= 5):
+        
+    if (selection == 1):
         print("You selected Kelvin to Farenheit")
         kelvin = input(("Enter degrees in Kelvin: "))
         kelvin = (1.8*(float(kelvin)-273.15)+32)
         print(f"Your degrees in Farenheit is: {kelvin}")
+        continue
+        
 
-    case '2':
+    elif (selection == 2):
         print("You selected Farenheit to Kelvin")
         farenheit = input(("Enter degrees in Farenheit: "))
         farenheit = (((float(farenheit)-32)/1.8)+273.15)
         print(f"Your degrees in Kelvin is {farenheit}")
-        
-    case '3':
+        continue
+
+    elif (selection == 3):
         print("You selected Meters to Yards")
         meters = input(("Enter degrees in Meters: "))
         meters = (float(meters)*1.094)
         print(f"Your length in Yards is {meters}")
+        continue
 
-    case '4':
+    elif (selection == 4):
         print("You selected Yards to Meters")
         yards = input(("Enter degrees in Yards: "))
         yards = (float(yards)/1.094)
         print(f"Your length in Meters is {yards}")
+        continue
 
-    case '5':
+    elif (selection == 5):
         print("Thank you for using converter, goodbye.")
-
+        break
